91163e8c8ef569ca22b46deb6d8fb5cbbaf3f8cd
Maksim Sviridov <sviridov@umich.edu>
2025-03-17 20:30:36 -0400

    added urlqueue

---
+++ UrlQueue.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "utils/vector.h"
+// #include "utils/HashTable.h"
+#include "utils/ParsedUrl.h"
+// #include "utils/string.h"
+#include <string>
+
+#include <queue>
+
+// using utils::vector;
+using std::string;
+
+
+// Data Structure that abstracts random K access to a queue of URLs
+class UrlQueue {
+
+    private:
+        utils::vector<string> urls;
+        // HashTable<int, int> index_map;
+
+        //? CAN THIS BE A VECTOR ?
+        std::queue<std::string> urlPool;
+
+    
+        void fillUrlPool() {
+            // select random K urls from urls and add them to urlPool
+            
+            // TODO: select N links and statically rank them to select the top K
+
+            const int k = std::min(urls.size(), DEFAULT_POOL_SIZE);
+            
+            for (int i = 0; i < k; ++i) {
+                // select a random index
+                const unsigned int randomIndex = rand() % urls.size();
+                std::string selectedUrl = urls[randomIndex];
+                urlPool.push(selectedUrl);
+
+                // swap the selected url with the last url in the vector
+                std::swap(urls[randomIndex], urls[urls.size() - 1]);
+                urls.popBack(); 
+            }
+        }
+
+    public:
+
+        static const size_t DEFAULT_POOL_SIZE = 10;
+
+        UrlQueue() : urls(), urlPool() {
+            
+         }
+
+        void addUrl(const std::string &url) {
+            urls.pushBack(url);
+        }
+
+        std::string getNextUrl() {
+
+            // while (urlPool.empty() and urls.empty()) {
+                // wait
+            // }
+
+            if (urlPool.empty() and urls.empty()) {
+                throw std::runtime_error("No URLs available");
+            }
+
+            if (urlPool.empty()) {
+                fillUrlPool();
+            }
+
+            std::string nextUrl = urlPool.front();
+            urlPool.pop();
+            return nextUrl;
+        }
+
+        inline bool empty() const {
+            return urls.empty() and urlPool.empty();
+        }
+
+};
+
--- frontier.h
+++ frontier.h
@@ -1,27 +1,29 @@
 #pragma once
 
-#include <queue>
+// #include <queue>
 #include "BloomFilter.h"
 
 #include "ReaderWriterLock.h"
 #include <pthread.h>
-
+#include "frontier/UrlQueue.h"
 
 
 class ThreadSafeFrontier {
 
     private:
-        std::queue<std::string> frontier_queue; 
+        // std::queue<std::string> frontier_queue;
+        UrlQueue frontier_queue; 
         Bloomfilter bloom_filter;
 
         // pthread_mutex_t lock;
         ReaderWriterLock rw_lock;
-        pthread_cond_t cv;
+        pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
 
     public:
-        ThreadSafeFrontier( int num_objects, double false_positive_rate ) : bloom_filter(num_objects, false_positive_rate) { 
+        ThreadSafeFrontier( int num_objects, double false_positive_rate ) : 
+        frontier_queue(), bloom_filter(num_objects, false_positive_rate), rw_lock() { 
             // pthread_mutex_init(&lock, nullptr); 
-            rw_lock = ReaderWriterLock();
+            // rw_lock = ReaderWriterLock();
         }
 
         void insert( const std::string &s ) {
@@ -29,7 +31,8 @@ class ThreadSafeFrontier {
                 WithWriteLock wl(rw_lock); 
                 
                 if ( !bloom_filter.contains(s) ) {
-                    frontier_queue.push(s);
+                    // frontier_queue.push(s);
+                    frontier_queue.addUrl(s);
                     bloom_filter.insert(s);
                 }
             }
@@ -45,8 +48,9 @@ class ThreadSafeFrontier {
                     pthread_cond_wait(&cv, &rw_lock.write_lock); // Wait for a URL to be available
                 }
 
-                std::string url = frontier_queue.front();
-                frontier_queue.pop();
+                // std::string url = frontier_queue.front();
+                // frontier_queue.pop();
+                std::string url = frontier_queue.getNextUrl();
                 return url;
             }
         }
